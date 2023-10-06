abd2973175c6e8a5229c1e5034d6481d0c06250b
Zeemilk <Mr.leszeemilk@Gmail.com>
2023-10-06 16:38:51 +0700

    ตายกันให้หมด

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    
+    <InputPage/>
+
+    <FunctionPage/>
+  </div>
+  
+</template>
+
+
+<script>
+import InputPage from 'src/pages/InputPage.vue'
+import FunctionPage from 'src/pages/FunctionPage.vue'
+
+export default {
+  name: 'HtmlComponent',
+  components: {
+    InputPage,FunctionPage
+  },
+  data() {
+    return {
+      title: 'Chanapai Weera 6504101316',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
